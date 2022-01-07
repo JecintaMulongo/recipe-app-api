d3d04e006a1b60fb20e1814ff30fd10397efe188
jecinta <jecinta.mulongo@8teq.co.ke>
2022-01-07 10:38:16 +0300

    recipe model

--- app/coresapp/models.py
+++ app/coresapp/models.py
@@ -61,3 +61,19 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object0"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    time_minute = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=2555, blank=True)
+    ingredient = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
